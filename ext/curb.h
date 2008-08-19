7b69342ba874d60aabb40a39678b2c48b5a35cae
Phillip Toland <ptoland@thehive.com>
2008-08-19 17:01:17 -0500

    Revert to the version scheme that I had been using previously.

--- ext/curb.h
+++ ext/curb.h
@@ -19,12 +19,12 @@
 #include "curb_macros.h"
 
 // These should be managed from the Rake 'release' task.
-#define CURB_VERSION   "0.2.1"
-#define CURB_VER_NUM   200
+#define CURB_VERSION   "0.1.4.2"
+#define CURB_VER_NUM   142
 #define CURB_VER_MAJ   0
-#define CURB_VER_MIN   2
-#define CURB_VER_MIC   0
-#define CURB_VER_PATCH 0
+#define CURB_VER_MIN   1
+#define CURB_VER_MIC   4
+#define CURB_VER_PATCH 2
 
 
 // Maybe not yet defined in Ruby
